4bcc02f66a3b8ed999585cf9e97f5cf15a0f7bf7
ppedvLukasK <lukask@ppedv.de>
2023-03-21 13:53:17 +0100

    M007

---
+++ M007/Fenster.cs
@@ -0,0 +1,139 @@
+﻿namespace M007
+{
+	internal class Fenster
+	{
+		#region Variable + Get/Set
+		private double laenge; //Felder sollten nicht von außen angreifbar sein (nur über Methoden)
+
+		/// <summary>
+		/// Gibt die Länge des Fensters zurück.
+		/// </summary>
+		/// <returns>Die Länge des Fensters in Meter.</returns>
+		public double GetLaenge()
+		{
+			return laenge;
+		}
+
+		/// <summary>
+		/// Setzt die Länge des Fensters auf einen neuen Wert.
+		/// </summary>
+		/// <param name="neueLaenge">Die neue Länge des Fensters in Meter (0 bis 10).</param>
+		public void SetLaenge(double neueLaenge)
+		{
+			if (neueLaenge > 0 && neueLaenge < 10) //Überprüfung machen bevor die Länge gesetzt wird
+			{
+				laenge = neueLaenge;
+			}
+			else
+			{
+				Console.WriteLine("Neue Länge zu klein/groß");
+			}
+		}
+		#endregion
+
+		#region Properties
+		private double breite;
+
+		public double Breite
+		{
+			get //Äquivalent zur Get-Methode
+			{
+				return breite;
+			}
+			set //Äquivalent zur Set-Methode
+			{
+				if (value > 0 && value < 10)
+					breite = value; //value: Der neue Wert (Äquivalent zu neueLaenge in SetLaenge)
+				else
+					Console.WriteLine("Neue Breite ist zu klein/groß");
+			}
+		}
+
+		public double BreiteKurz
+		{
+			get => breite; //Get-Accessor in Kurzform durch Expression Body
+			set //Äquivalent zur Set-Methode
+			{
+				if (value > 0 && value < 10)
+					breite = value; //value: Der neue Wert (Äquivalent zu neueLaenge in SetLaenge)
+				else
+					Console.WriteLine("Neue Breite ist zu klein/groß");
+			}
+		}
+
+		private FensterStatus status = FensterStatus.Geschlossen; //Hier Standardwert auf Variablenebene setzen
+
+		public FensterStatus Status
+		{
+			get => status;
+			private set //private set: nicht von außen setzbar (nur innerhalb der Klasse)
+			{
+				status = value;
+			}
+		}
+
+		/// <summary>
+		/// Eine Methode die das Fenster öffnet.
+		/// </summary>
+		public void FensterOeffnen()
+		{
+			if (Status != FensterStatus.Offen)
+				Status = FensterStatus.Offen; //private set ist hier sichtbar
+			else
+				Console.WriteLine("Fenster ist bereits geöffnet.");
+		}
+
+		//Get-Only Property
+		public double Area
+		{
+			get
+			{
+				return laenge * breite;
+			}
+		}
+
+		public double Area2
+		{
+			get => laenge * breite;
+		}
+
+		public double Area3 => laenge * breite;
+
+		public double CalcArea() => laenge * breite;
+
+		public int Scheibenanzahl { get; private set; } = 2; //Standardwert setzen auf Propertyebene
+		#endregion
+
+		#region Konstruktor
+		public Fenster() { } //Leerer Konstruktor wird gelöscht, wenn ein eigener Konstruktor erstellt wird
+
+		/// <summary>
+		/// Der Initialcode des Fensters.
+		/// </summary>
+		/// <param name="laenge">Die Länge des Fensters</param>
+		/// <param name="breite">Die Breite des Fensters</param>
+		public Fenster(double laenge, double breite) //Konstruktor: Code der bei Erstellung des Objekts ausgeführt wird (new)
+		{
+			this.laenge = laenge; //this: nach oben (in die Klasse) greifen (hier laenge = laenge setzt den Wert auf den Parameter selbst)
+			this.breite = breite;
+		}
+
+		public Fenster(double laenge, double breite, int scheiben) : this(laenge, breite) //Konstruktoren verketten (wenn dieser Konstruktor verwendet wird, wird auch der obere verwendet)
+		{
+			Scheibenanzahl = scheiben;
+		}
+		#endregion
+
+		//~ + Tab + Tab
+		~Fenster()
+		{
+			Console.WriteLine($"Fenster eingesammelt {GetHashCode()}"); //Wird aufgerufen wenn der Garbage Collector das Objekt einsammelt
+			//GetHashCode(): Zeigt die Speicheradresse
+		}
+	}
+
+	public enum FensterStatus
+	{
+		Offen, Gekippt, Geschlossen
+	}
+}
---
+++ M007/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace M007
+{
+	internal class Program
+	{
+		public static string Info = "Eine Information";
+
+		static void Main(string[] args)
+		{
+			#region GC
+			for (int i = 0; i < 5; i++)
+			{
+				Fenster f = new Fenster();
+				Console.WriteLine(f.GetHashCode());
+			}
+
+			GC.Collect(); //Hier GC erzwingen
+			GC.WaitForPendingFinalizers(); //Warte auf alle Destruktoren
+
+			Fenster f1 = new();
+			Fenster f2 = new();
+			if (f1 == f2)
+			{
+				//Hier werden die HashCodes (GetHashCode()) der beiden Objekte verglichen
+			}
+			#endregion
+
+			#region Static
+			//Statische Member müssen ohne Objekte arbeiten
+			//new Program().Main(args); //nicht möglich, da statisch
+			//Program.Main(args); //Statische Methoden müssen über den Klassennamen angesprochen werden
+
+			//Console c = new Console(); //Von statischen Klassen kann kein Objekt erstellt werden
+			//c.WriteLine();
+			//Console.WriteLine();
+
+			Console.WriteLine(Program.Info);
+
+			//StaticTest(); //nicht möglich, da nicht statisch
+			new Program().StaticTest();
+
+			//DateTime.Now
+
+
+			Program[] p = new Program[10];
+			for (int i = 0; i < p.Length; i++)
+			{
+				p[i] = new Program();
+			}
+			Program.Info = "Zwei Informationen"; //Wird bei allen Objekten angepasst
+			#endregion
+
+			#region Werte-/Referenztypen
+			//Wertetyp
+			int original = 5;
+			int x = original;
+			original = 10;
+			Console.WriteLine(original.GetHashCode());
+			Console.WriteLine(x.GetHashCode()); //Unterschiedliche HashCodes
+
+			//Referenztyp
+			Fenster f3 = new();
+			Fenster f4 = f3;
+			f3.Breite = 5;
+			Console.WriteLine(f3.GetHashCode());
+			Console.WriteLine(f4.GetHashCode()); //Gleiche HashCodes, zeigen auf das selbe Objekt im RAM
+
+			//class
+			//Referenztyp
+			//==, != werden Speicheradressen verglichen
+			//Zuweisungen haben Referenzen statt Kopien
+
+			//struct
+			//Wertetyp
+			//==, != werden die Werte in dem Objekt verglichen
+			//Zuweisungen haben Kopien statt Referenzen
+			#endregion
+
+			#region Null
+			Fenster f5; //Standardmäßig null (es hängt kein Wert daran)
+			f5 = null; //Referenz zum Objekt trennen
+			//f5.FensterOeffnen(); //Nicht vorhandenes Fenster kann nicht geöffnet werden
+
+			if (f5 != null) //Schauen ob das Objekt existiert
+			{
+				f5.FensterOeffnen();
+			}
+
+			if (f5 == null)
+			{
+				f5 = new Fenster();
+			}
+
+			if (f5 is null && f5 is not null) //Selbiges wie == und !=
+			{
+
+			}
+			#endregion
+		}
+
+		public void StaticTest()
+		{
+			Console.WriteLine(Program.Info);
+		}
+	}
+}
